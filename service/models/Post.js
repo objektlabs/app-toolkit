0cc2434de3030ce9ef8e8552937cb5d6ca808316
jn42lm1 <jn42lm1+github@gmail.com>
2020-06-20 17:02:57 +0200

    Based files for initial experiment.

---
+++ service/models/Post.js
@@ -0,0 +1,42 @@
+export class Post {
+
+	constructor(id, title, body, userId) {
+
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.userId = userId;
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	set id(value) {
+		this._id = value;
+	}
+
+	get title() {
+		return this._title;
+	}
+
+	set title(value) {
+		this._title = value;
+	}
+
+	get body() {
+		return this._body;
+	}
+
+	set body(value) {
+		this._body = value;
+	}
+
+	get userId() {
+		return this._userId;
+	}
+
+	set userId(value) {
+		this._userId = value;
+	}
+}
